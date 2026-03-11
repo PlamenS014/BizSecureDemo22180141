6c3efc593e4400b7a0f40ab1806d0f11308da254
plamenstanislavov <plamenstanislavov@gmail.com>
2026-03-11 15:41:00 +0200

    excerise 4

--- BizSecureDemo22180141/Models/AppUser.cs
+++ BizSecureDemo22180141/Models/AppUser.cs
@@ -4,4 +4,6 @@ public class AppUser
     public int Id { get; set; }
     public string Email { get; set; } = "";
     public string PasswordHash { get; set; } = "";
+    public int? FailedLogins { get; set; }
+    public DateTime? LockoutUntilUtc { get; set; }
 }
