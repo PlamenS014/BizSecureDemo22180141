54e8018d794d0ed68553d21f5639aada1c811eee
plamenstanislavov <plamenstanislavov@gmail.com>
2026-03-11 14:17:53 +0200

    Add project files.

---
+++ BizSecureDemo22180141/Models/AppUser.cs
@@ -0,0 +1,7 @@
+﻿namespace BizSecureDemo22180141.Models;
+public class AppUser
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+}
---
+++ BizSecureDemo22180141/ViewModels/CreateOrderVm.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BizSecureDemo22180141.ViewModels;
+public class CreateOrderVm
+{
+    [Required, MaxLength(300)]
+    public string Title { get; set; } = "";
+    [Required]
+    public decimal Amount { get; set; }
+}
