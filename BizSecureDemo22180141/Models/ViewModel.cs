27fe768ee610a1892ed43c4a7a27749048ded3f9
plamenstanislavov <plamenstanislavov@gmail.com>
2026-04-19 21:55:55 +0300

    19.4

---
+++ BizSecureDemo22180141/Models/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BizSecureDemo22180141.ViewModels
+{
+    public class ReplayDemoVm
+    {
+        public string Nonce { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public string? Message { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; } = 100;
+        public string Token { get; set; } = "SECRET123";
+    }
+}
